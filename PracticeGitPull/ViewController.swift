dcb09d5132539119ea06c8e32585104f1c12c2b2
0102johnny <rt2824.recruit.1022@gmail.com>
2023-04-07 20:07:39 +0900

    Initial Commit

---
+++ PracticeGitPull/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PracticeGitPull
+//
+//  Created by Johnny Toda on 2023/04/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
